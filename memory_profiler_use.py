60d839ccd0b7bb645693bbca09806b7ed34b009b
Romaïssa <romaissa.mekhzouni@ensta-paristech.fr>
2021-06-25 17:31:39 +0200

    Add cpu profiler (10 most functions using time)

--- memory_profiler_use.py
+++ memory_profiler_use.py
@@ -26,6 +26,7 @@ def add_descriptors(path) :
 if __name__ == "__main__":
     # can also be given by the front end interface for example
     file_path = '/home/romaissa/Documents/HerHackathon/GreenCodeEvaluator/empty_bad_functions.py'
+    # file_path = '/home/romaissa/Documents/HerHackathon/GreenCodeEvaluator/python_code/Scrape_HackerNews.py'
 
     # get updated code file
     new_path = add_descriptors(file_path)
@@ -35,8 +36,22 @@ if __name__ == "__main__":
 
     # launch mprof commands
     os.system('mprof run '+new_path.rsplit('/', 1)[1])
+
+    # launch cprof commands (cpu time profiling) sorted by time 
+    os.system('python -m cProfile -s time '+file_path+"> "+new_path.rsplit('/', 1)[0]+"/cprof.txt")
+
+    # print the first 10 lines using most CPU time
+    print("----- First 10 most using CPU elements")
+    with open(new_path.rsplit('/', 1)[0]+"/cprof.txt") as myfile:
+        while not 'Ordered by' in next(myfile):
+            pass
+        for x in range(10) : 
+            head = next(myfile)
+            print(head)
+
     os.system('mprof plot')
 
     # remove file added
     os.system('rm mprofile*')
     os.system('rm '+new_path)
+    os.system('rm '+new_path.rsplit('/', 1)[0]+"/cprof.txt")
