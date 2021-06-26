71501df344f9bc3f5c9748996ee2ae4c43b91cb7
NupurKul05 <50444628+NupurKul05@users.noreply.github.com>
2021-06-26 14:41:30 +0200

    Update with memory profiler
    1. Added profiler for functions
    2. Implemented line-by-line profiling

--- unused_code_checker.py
+++ unused_code_checker.py
@@ -1,13 +1,28 @@
 """ Goal of this blackbox code :
 1. Display unused imports, variables and functions from input code
-2. Display duplicated variables
-3. Determine algorithmic complexity of input code
-4. Code section with high energy cost (CPU and RAM usage)
+2. Line-by-line memory usage
 """
+
 import os
+
+def add_profiler(file_path) :
+    new_path = file_path+"_with_descriptors.py"
+    fin = open(file_path, "r")
+    fout = open(new_path, "w")
+
+    for line in fin:
+        fout.write(line.replace('def ', '@profile \ndef '))
+
+    fin.close()
+    fout.close()
+    return new_path
 
 if __name__ == "__main__":
     file_path = 'demo.py'
 
     # 1. Check unused imports, variables and functions (Used 'vulture' package)
     os.system('vulture ' + file_path)
+    
+    # 2 . Line-by-line memory usage
+    new_path = add_profiler(file_path)
+    os.system('python -m memory_profiler ' + new_path)
