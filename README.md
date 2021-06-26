8afe52595a149bd5a7d400645654d47c007b5523
martasls <martasls95@gmail.com>
2021-06-26 01:00:14 +0100

    Add some experiments

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # GreenCodeEvaluator
+
+This is our proposal for Mercedes' HerHackaton Challenge. 
+
+## Installation
+
+```commandline
+pip install git+https://github.com/martasls/GreenCodeEvaluator.git
+```
+
 Repository for the HerHackathon 2021
 
 
--	Every participant should have her own GitHub account
--	For each team, a participant forks this repo and gives her team mates access rights accordingly
+- Every participant should have her own GitHub account
+- For each team, a participant forks this repo and gives her teammates access rights accordingly
 - Each team works on their own repository. It is probably beneficial to be familiar with GitHub Actions to have a common development environment
 - At the end, teams can submit pull requests back to this repo, where appropriate. Please note that we ask you to sign our [Contributor License Agreement](https://github.com/Daimler/daimler-foss/blob/master/cla/2019-09-11_Daimler_FOSS_CLA_DaimlerTSS.pdf) and then submit to [Daimler TSS CLA team](mailto:CLA-TSS@daimler.com) so that we can accept your contribution.
 
--- green_code_evaluator/commands/analyze.py
+++ green_code_evaluator/commands/analyze.py
@@ -1,56 +1,98 @@
 import os
+import re
+
 import click
 
 
 # function to change the input code with descriptors
-def add_descriptors(file_path) :
+def add_descriptors(file_path: str):
     new_path = file_path.rsplit('.', 1)[0]+"_with_descriptors.py"
-    #input file
+    # input file
     fin = open(file_path, "rt")
-    #output file to write the result to
+    # output file to write the result to
     fout = open(new_path, "wt")
 
-    #for each line in the input file
+    # for each line in the input file
     for line in fin:
-        fout.write(line.replace("def ", "@profile \ndef "))
+        if re.match(r"\s", line):
+            leading_spaces = len(line) - len(line.lstrip())
+            fout.write(line.replace("def ", "@profile\n" + " " * leading_spaces + "def "))
+        else:
+            fout.write(line.replace("def ", "@profile\ndef "))
 
-    #close input and output files
+    # close input and output files
     fin.close()
     fout.close()
     return new_path
 
 
 @click.command(name="analyze", help="Analyzes some python code")
-@click.argument("python_script_path", nargs=1)
-def command(python_script_path: str):
-    print(python_script_path)
-    # get updated code file
-    new_path = add_descriptors(python_script_path)
+@click.argument("input_path", nargs=1)
+@click.argument("results_directory_path", nargs=1)
+def command(input_path: str, results_directory_path: str):
+    """
 
-    # change directory
-    os.chdir(new_path.rsplit('/', 1)[0])
+    Args:
+        input_path: a folder or file path to be analyzed
+        results_directory_path: directory where results are to be saved
 
-    # launch mprof commands
-    os.system('mprof run ' + new_path.rsplit('/', 1)[1])
+    """
+    if not os.path.exists(results_directory_path):
+        os.makedirs(results_directory_path)
 
-    # launch cprof commands (cpu time profiling) sorted by time
-    os.system('python -m cProfile -s time ' + python_script_path + "> " + new_path.rsplit('/', 1)[0] + "/cprof.txt")
+    if os.path.isfile(input_path):
+        new_path = add_descriptors(input_path)
 
-    # print the first 10 lines using most CPU time
-    print("----- First 10 most using CPU elements")
-    with open(new_path.rsplit('/', 1)[0] + "/cprof.txt") as myfile:
-        while not 'Ordered by' in next(myfile):
-            pass
-        for x in range(10):
-            head = next(myfile)
-            print(head)
+        # launch mprof commands
+        os.system("mprof run " + new_path)
 
-    os.system('mprof plot')
+        # launch cprof commands (cpu time profiling) sorted by time
+        os.system("python -m cProfile -s time " + input_path + "> " + os.path.join(results_directory_path, "cprof.txt"))
 
-    # remove file added
-    os.system('rm mprofile*')
-    os.system('rm ' + new_path)
-    os.system('rm ' + new_path.rsplit('/', 1)[0] + "/cprof.txt")
+        # print the first 10 lines using most CPU time
+        print("----- First 10 most using CPU elements")
+        with open(os.path.join(results_directory_path, "cprof.txt")) as f_ref:
+            while "Ordered by" not in next(f_ref):
+                pass
+            for x in range(10):
+                head = next(f_ref)
+                print(head)
+
+        os.system('mprof plot')
+
+        # remove file added
+        os.system('rm mprofile*')
+        os.system('rm ' + new_path)
+        os.system('rm ' + new_path.rsplit('/', 1)[0] + "/cprof.txt")
+    else:
+        for root, dirs, files in os.walk(input_path):
+            for file in files:
+                if file.endswith(".py"):
+                    file_path = os.path.join(root, file)
+                    # get updated code file
+                    new_path = add_descriptors(file_path)
+
+                    # launch mprof commands
+                    os.system("mprof run " + new_path)
+
+                    # launch cprof commands (cpu time profiling) sorted by time
+                    os.system("python -m cProfile -s time " + file_path + "> " + os.path.join(results_directory_path, "cprof.txt"))
+
+                    # print the first 10 lines using most CPU time
+                    print("----- First 10 most using CPU elements")
+                    with open(os.path.join(results_directory_path, "cprof.txt")) as f_ref:
+                        while "Ordered by" not in next(f_ref):
+                            pass
+                        for x in range(10):
+                            head = next(f_ref)
+                            print(head)
+
+                    os.system('mprof plot')
+
+                    # remove file added
+                    os.system('rm mprofile*')
+                    os.system('rm ' + new_path)
+                    os.system('rm ' + new_path.rsplit('/', 1)[0] + "/cprof.txt")
 
 
 if __name__ == "__main__":
