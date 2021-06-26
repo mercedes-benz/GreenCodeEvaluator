af8a7d172470c6c4519c2fac3cef366029735b65
CristinaBulnes <cristinabulnesmaya@gmail.com>
2021-06-26 21:23:21 +0200

    feat: change memory result path and get function result names

--- frontend/src/components/Memory.js
+++ frontend/src/components/Memory.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import memoryImg from '../img/memory_usage.png'
+import memoryImg from '../../public/memory_usage.png'
 
 const Memory = () => {
-    
-  
-      const title = 'Memory'
+    const title = 'Memory';
     return (
         <>
         <h2 className='result-title'>{title}</h2>
-            <div>
+            <div className='result-container'>
             <img src={memoryImg} alt='img graph'/>
         </div>
         </>
