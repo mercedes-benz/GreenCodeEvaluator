c9e3de2bf02e19406379fbef1b170e2f98788dad
PraiseThampiV <praisethampi5@gmail.com>
2021-06-27 08:15:31 +0200

    sys info

--- frontend/src/components/SysInfo.js
+++ frontend/src/components/SysInfo.js
@@ -1,69 +1,67 @@
-import sysdata from "../out_json_img/sysinfo.json";
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import ReactDOM from "react-dom";
 import "../assets/static/css/SysinfoStyle.css";
-import ListGroup from "react-bootstrap/ListGroup";
 
-class TableComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      json: null,
-    };
-  }
-  getData = () => {
-    fetch("sysinfo.json", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (result) {
-        this.setState({ json: result });
-      });
-  };
-  // componentDidMount() {
-  //     this.setState((prevState) => {
-  //         return {
-  //             json: tampildata()
-  //         }
-  //     })
-  // }
 
-  render() {
-    if (this.state.json)
-    return (
-      <div>
-        <table
+const SysInfo = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+      getData();
+    },[])
+
+  const getData=()=>{
+      fetch('sysinfo.json'
+      ,{
+        headers : {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+      )
+        .then(function(response){
+          return response.json();
+        }).then (function(result) {
+            console.log(result)
+              setData(result);
+        })
+    }
+    console.log("sys info",data)
+    const title = 'Your System'
+  return (
+      (data && data.length === 0 ? <div>Loading...</div> :
+        <>
+        <h2 className='result-title'>{title}</h2>
+        <div className='result-container'>
+        <table 
           style={{
             border: "1px solid black",
+            marginLeft: "auto",
+            marginRight: "auto"
           }}
         >
           <tbody style={{ color: "blue" }}>
             <tr>
               <td>Platform</td>
-              <td>{this.state.json.platform}</td>
+              <td>{data.platform}</td>
             </tr>
             <tr>
               <td>Platform Release</td>
-              <td>{this.state.json.platform_release}</td>
+              <td>{data.platform_release}</td>
             </tr>
             <tr>
               <td>Platform Version</td>
-              <td>{this.state.json.platform_version}</td>
+              <td>{data.platform_version}</td>
             </tr>
             <tr>
               <td>Processor</td>
-              <td>{this.state.json.processor}</td>
+              <td>{data.processor}</td>
             </tr>
             <tr>
               <td>RAM</td>
-              <td>{this.state.json.ram}</td>
+              <td>{data.ram}</td>
             </tr>
-            {/* {this.state.json.map((data, i) => {
+            {/* {this.state.dSys.map((data, i) => {
                             return (
                                 <tr key={i}>
                                     <td>{data.platform}</td>
@@ -73,54 +71,13 @@ class TableComponent extends React.Component {
                         })} */}
           </tbody>
         </table>
+      
       </div>
-    )
-    else
-    return (<div>Loading...</div>)
-    // return
-    // ((this.state.json ? (
-    //   <div>Loading...</div>
-    // ) : (
-    //   <div>
-    //     <table
-    //       style={{
-    //         border: "1px solid black",
-    //       }}
-    //     >
-    //       <tbody style={{ color: "blue" }}>
-    //         <tr>
-    //           <td>Platform</td>
-    //           <td>{this.state.json.platform}</td>
-    //         </tr>
-    //         <tr>
-    //           <td>Platform Release</td>
-    //           <td>{this.state.json.platform_release}</td>
-    //         </tr>
-    //         <tr>
-    //           <td>Platform Version</td>
-    //           <td>{this.state.json.platform_version}</td>
-    //         </tr>
-    //         <tr>
-    //           <td>Processor</td>
-    //           <td>{this.state.json.processor}</td>
-    //         </tr>
-    //         <tr>
-    //           <td>RAM</td>
-    //           <td>{this.state.json.ram}</td>
-    //         </tr>
-    //         {/* {this.state.json.map((data, i) => {
-    //                         return (
-    //                             <tr key={i}>
-    //                                 <td>{data.platform}</td>
-    //                                 <td>{data.lastname}</td>
-    //                             </tr>
-    //                         )
-    //                     })} */}
-    //       </tbody>
-    //     </table>
-    //   </div>
-    // )));
-  }
+      </>
+          )
+      
+  );
+
 }
 
-export default TableComponent;
+export default SysInfo;
